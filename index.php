334422b914546fc22fd85129cc6188e2f762a820
stefan tlach <stefan.tlach@gmail.com>
2011-09-12 20:45:55 +0200

    test 12.9.2011

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<head>
+<title>EveryCook "The worlds best recipe database"</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+<meta name="author" contest="Stefan Tlach">
+</head>
+<body>
+<div> <img id="bg" src="pics/bg.png" alt="" title="" </div>
+</body>
