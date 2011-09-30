a257fd185d3156d34195bb77c21ee0b9ef4b990f
stefan tlach <stefan.tlach@gmail.com>
2011-09-30 12:47:34 +0200

    Does it work?

--- index.php
+++ index.php
@@ -6,16 +6,23 @@
 		<meta charset="utf-8">
 		<script type="text/javascript" src="js/index.js"></script>
 		<script type="text/javascript">
-		
-		var textparts = new Array();
-		function getlang(langage) {
-			$.getScript('langtest.php?language='+language, function() {
-				document.getElementById("login").innerHTML=textparts['login'];
-				
+		var view = "0";
+		var fields = new Array();
+		function getlang(language) {
+			$.getScript('langmenu.php?language='+language, function() {
+				document.getElementById("login").innerHTML=fields['IME_LOGIN'];
+				document.getElementById("settings").innerHTML=fields['IME_SETTINGS'];
+				document.getElementById("lang").innerHTML=fields['IME_LANGSEL'];	
 			});
-			alert(textparts["login"]);
+			$.getScript('langview.php?language='+language+'&view='+view, function() {
+				document.getElementById("sr").innerHTML=fields['ITE_BOTL'];
+				document.getElementById("sf").innerHTML=fields['ITE_BOTM'];
+				document.getElementById("tcm").innerHTML=fields['ITE_BOTR'];
+				document.getElementById("text_mf").innerHTML=fields['ITE_MIDL'];
+				document.getElementById("mf").innerHTML=fields['ITE_MIDR'];
+			});
 		}
-		
+		getlang('English');
 		$(document).ready(function() {
 			$("#index_lang").hover(function() {
 				$("#index_lang_pick").addClass("index_lang_v");
@@ -23,105 +30,49 @@
 				$("#index_lang_pick").removeClass("index_lang_v");
 			});
 		});
-		
 		</script>
 	</head>
 	<body>
 		<img src="pics/bg.png" alt="Background" id="index_bg"/>
 		<div id="index_content">
-			<a href="log.php">
-				<div id="index_log">
-					<div class="index_text_middle">
-						<div>
-							<span id="login"></span>
-						</div>
-					</div>
-				</div>
-			</a>
-			<a href="set.php">
-				<div id="index_set">
-					<div class="index_text_middle">
-						<div>
-							Settings
-						</div>
-					</div>
-				</div>
-			</a>
-			<div id="index_mf">
-			
-			</div>
-			<div id="index_text_mf">
-				<div id="index_text_mf_t">
-					Text
-				</div>
-				<a href="pymn.php">
-					<div id="index_pymn">
-						<div class="index_text_middle">
-							<div>
-								Plan Your Meal Now
-							</div>
-						</div>
-					</div>
-				</a>
-			</div>
-			<a href="sr.php">
-				<div id="index_sr">
-					<div class="index_text_middle">
-						<div>
-							Search Recipe
-						</div>
-					</div>
-				</div>
-			</a>
-			<a href="sf.php">
-				<div id="index_sf">
-					<div class="index_text_middle">
-						<div>
-							Search Food
-						</div>
-					</div>
-				</div>
-			</a>
-			<a href="tcm.php">
-				<div id="index_tcm">
-					<div class="index_text_middle">
-						<div>
-							The Cooking Machine
-						</div>
-					</div>
-				</div>
-			</a>
+			<span id="login"></span>
+			<span id="settings"></span>
+			<span id="mf"></span>
+			<span id="text_mf"></span>
+			<span id="sr"></span>
+			<span id="sf"></span>
+			<span id="tcm"></span>
 			<div id="index_lang">
 				<div class="index_text_middle">
 					<div>
-						Language
+						<span id="lang"></span>
 					</div>
 				</div>
 				<div id="index_lang_pick" class="index_lang_h">
 					<?php
-						$dir = "languages";	//quell ordner
-						$lang_array = Array();	//ziel Array
-						if(is_dir($dir)) {	//existiert ordner?
-							$handle = opendir($dir);	//öffne ordner
-							if(is_resource($handle)) {	//ordner offen?
-								while($file=readdir($handle)) {	//daten auslesen
-									if($file != "." && $file != "..")
-										array_push($lang_array, $file);
-								}
-							}else{
-								$lang_array[0] = "fehler opendir";
-							}
-						}else{
-							$lang_array[0] = "fehler existdir";
-						}
-						foreach($lang_array AS $language)
-						{
-							if (strrpos($language, '~')===false){
-								echo '<a href="#" onClick="getlang('.$language.');">';
-								echo substr($language,0,-4);
-								echo '</a>';
-								echo "<br>";
-							}
+						$lang_array;			
+						/* Datenbankserver - In der Regel die IP */
+						$db_server = 'localhost';
+						/* Datenbankname */
+						$db_name = '30608_everycook';
+						/* Datenbankuser */
+						$db_user = 'root';
+						/* Datenbankpasswort */
+						$db_passwort = 'test';
+							 
+						/* Erstellt Connect zu Datenbank */
+						$db = @ mysql_connect ( $db_server, $db_user, $db_passwort );
+						
+						$db_select = @ mysql_select_db( $db_name );
+						
+						$sql = 'SELECT * FROM interface_menu ';
+						
+						$ergebnis = mysql_query( $sql );
+						while ($row=mysql_fetch_assoc($ergebnis)){
+							echo '<a href="#" onClick="getlang(\''.$row['IME_LANGNAME'].'\');">';
+							echo $row['IME_LANGNAME'];
+							echo '</a>';
+							echo "<br>";
 						}
 					?>
 				</div>
