efffa354286bcf802e0a24005897ddbb84065965
stefan tlach <stefan.tlach@gmail.com>
2011-10-01 14:20:21 +0200

    01.10.2011

--- index.php
+++ index.php
@@ -4,44 +4,88 @@
 		<title>EveryCook "The worlds best recipe database"</title>
 		<link rel="stylesheet" type="text/css" href="css/styles.css"/>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<script type="text/javascript" src="js/index.js"></script>
+		<script type="text/javascript" src="js/jquery-1.6.4.js"></script>
 		<script type="text/javascript">
-		var view = "0";
-		var fields = new Array();
-		function getlang(language) {
-			$.getScript('langmenu.php?language='+language, function() {
-				document.getElementById("login").innerHTML=fields['IME_LOGIN'];
-				document.getElementById("settings").innerHTML=fields['IME_SETTINGS'];
-				document.getElementById("lang").innerHTML=fields['IME_LANGSEL'];	
+			var view = "0";
+			var fields = new Array();
+			function getlang(language) {
+				$.getScript('langmenu.php?language='+language, function() {
+					$('#login').html(fields['IME_LOGIN']);
+					$('#settings').html(fields['IME_SETTINGS']);
+					$('#lang').html(fields['IME_LANGSEL']);	
+				});
+				$.getScript('langview.php?language='+language+'&view='+view, function() {
+					$('#sr').html(fields['ITE_BOTL']);
+					$('#sf').html(fields['ITE_BOTM']);
+					$('#tcm').html(fields['ITE_BOTR']);
+					$('#text_mf').html(fields['ITE_MIDL']);
+					$('#mf').html(fields['ITE_MIDR']);
+				});
+			}
+			getlang('English');
+			$(document).ready(function() {
+				$("#index_lang").hover(function() {
+					$("#index_lang_pick").addClass("index_lang_v");
+				},function(){
+					$("#index_lang_pick").removeClass("index_lang_v");
+				});
 			});
-			$.getScript('langview.php?language='+language+'&view='+view, function() {
-				document.getElementById("sr").innerHTML=fields['ITE_BOTL'];
-				document.getElementById("sf").innerHTML=fields['ITE_BOTM'];
-				document.getElementById("tcm").innerHTML=fields['ITE_BOTR'];
-				document.getElementById("text_mf").innerHTML=fields['ITE_MIDL'];
-				document.getElementById("mf").innerHTML=fields['ITE_MIDR'];
-			});
-		}
-		getlang('English');
-		$(document).ready(function() {
-			$("#index_lang").hover(function() {
-				$("#index_lang_pick").addClass("index_lang_v");
-			},function(){
-				$("#index_lang_pick").removeClass("index_lang_v");
-			});
-		});
 		</script>
 	</head>
 	<body>
 		<img src="pics/bg.png" alt="Background" id="index_bg"/>
 		<div id="index_content">
-			<span id="login"></span>
-			<span id="settings"></span>
-			<span id="mf"></span>
-			<span id="text_mf"></span>
-			<span id="sr"></span>
-			<span id="sf"></span>
-			<span id="tcm"></span>
+			<a href="#" OnClick="ShowLogin()">
+				<div id="index_log">
+					<div class="index_text_middle">
+						<div>
+							<span id="login"></span>
+						</div>
+					</div>
+				</div>
+			</a>
+			<a href="#" OnClick="ShowSettings()">
+				<div id="index_set">
+					<div class="index_text_middle">
+						<div>
+							<span id="settings"></span>
+						</div>
+					</div>
+				</div>
+			</a>
+			<div id="index_mf">
+				<span id="mf"></span>
+			</div>
+			<div id="index_text_mf"><div id="index_text_mf_t">
+				<span id="text_mf"></span>
+			</div>
+			<a href="#" OnClick="ShowView('1')">
+				<div id="index_sr">
+					<div class="index_text_middle">
+						<div>
+							<span id="sr"></span>
+						</div>
+					</div>
+				</div>
+			</a>
+			<a href="#" OnClick="ShowView('2')">
+				<div id="index_sf">
+					<div class="index_text_middle">
+						<div>
+							<span id="sf"></span>
+						</div>
+					</div>
+				</div>
+			</a>
+			<a href="#" OnClick="ShowView('3')">
+				<div id="index_tcm">
+					<div class="index_text_middle">
+						<div>
+							<span id="tcm"></span>
+						</div>
+					</div>
+				</div>
+			</a>
 			<div id="index_lang">
 				<div class="index_text_middle">
 					<div>
@@ -51,22 +95,14 @@
 				<div id="index_lang_pick" class="index_lang_h">
 					<?php
 						$lang_array;			
-						/* Datenbankserver - In der Regel die IP */
 						$db_server = 'localhost';
-						/* Datenbankname */
 						$db_name = '30608_everycook';
-						/* Datenbankuser */
 						$db_user = 'root';
-						/* Datenbankpasswort */
 						$db_passwort = 'test';
-							 
-						/* Erstellt Connect zu Datenbank */
 						$db = @ mysql_connect ( $db_server, $db_user, $db_passwort );
-						
 						$db_select = @ mysql_select_db( $db_name );
 						mysql_query("SET NAMES 'utf8'");
 						$sql = 'SELECT * FROM interface_menu ';
-						
 						$ergebnis = mysql_query( $sql );
 						while ($row=mysql_fetch_assoc($ergebnis)){
 							echo '<a href="#" onClick="getlang(\''.$row['IME_LANGNAME'].'\');">';
--- langview.php
+++ langview.php
@@ -1,42 +1,29 @@
 <?php
-$language=$_GET['language'];
-if($language=='English'){
-	$language = "EN";
-}
-else if($language=='Deutsch'){
-	$language = "DE";
-}
-else if($language=='Français'){
-	$language = "FR";
-}
-
-$view = $_GET['view'];
-
-/* Datenbankserver - In der Regel die IP */
-$db_server = 'localhost';
-/* Datenbankname */
-$db_name = '30608_everycook';
-/* Datenbankuser */
-$db_user = 'root';
-/* Datenbankpasswort */
-$db_passwort = 'test';
-         
-/* Erstellt Connect zu Datenbank */
-$db = @ mysql_connect ( $db_server, $db_user, $db_passwort );
-
-$db_select = @ mysql_select_db( $db_name );
-mysql_query("SET NAMES 'utf8'");
-
-$sql = 'SELECT * FROM interface_textes WHERE ITE_LANG = "'.$language.'" AND ITE_VIEW = "'.$view.'"';
-
-$ergebnis = mysql_query( $sql );
-
-$row=mysql_fetch_assoc($ergebnis);
-
-echo 'fields["ITE_BOTL"]="'.addcslashes ( $row['ITE_BOTL'], '"' ).'";';
-echo 'fields["ITE_BOTM"]="'.addcslashes ( $row['ITE_BOTM'], '"' ).'";';
-echo 'fields["ITE_BOTR"]="'.addcslashes ( $row['ITE_BOTR'], '"' ).'";';
-echo 'fields["ITE_MIDL"]="'.addcslashes ( $row['ITE_MIDL'], '"' ).'";';
-echo 'fields["ITE_MIDR"]="'.addcslashes ( $row['ITE_MIDR'], '"' ).'";';
+	$language=$_GET['language'];
+	if($language=='English'){
+		$language = "EN";
+	}
+	else if($language=='Deutsch'){
+		$language = "DE";
+	}
+	else if($language=='Français'){
+		$language = "FR";
+	}
+	$view = $_GET['view'];
+	$db_server = 'localhost';
+	$db_name = '30608_everycook';
+	$db_user = 'root';
+	$db_passwort = 'test';
+	$db = @ mysql_connect ( $db_server, $db_user, $db_passwort );
+	$db_select = @ mysql_select_db( $db_name );
+	mysql_query("SET NAMES 'utf8'");
+	$sql = 'SELECT * FROM interface_textes WHERE ITE_LANG = "'.$language.'" AND ITE_VIEW = "'.$view.'"';
+	$ergebnis = mysql_query( $sql );
+	$row=mysql_fetch_assoc($ergebnis);
+	echo 'fields["ITE_BOTL"]="'.addcslashes ( $row['ITE_BOTL'], '"' ).'";';
+	echo 'fields["ITE_BOTM"]="'.addcslashes ( $row['ITE_BOTM'], '"' ).'";';
+	echo 'fields["ITE_BOTR"]="'.addcslashes ( $row['ITE_BOTR'], '"' ).'";';
+	echo 'fields["ITE_MIDL"]="'.addcslashes ( $row['ITE_MIDL'], '"' ).'";';
+	echo 'fields["ITE_MIDR"]="'.addcslashes ( $row['ITE_MIDR'], '"' ).'";';
 
 ?>
