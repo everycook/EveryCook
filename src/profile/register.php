c4ed06f85f244cd29276db309b406a6412ff37c1
Stefan Tlach <stefan.tlach@gmail.com>
2011-10-26 21:04:49 +0200

    new structure

---
+++ src/profile/register.php
@@ -0,0 +1,12 @@
+<?php
+	include 'lang.php';
+	$fname=$_GET['fname'];
+	$lname=$_GET['lname'];
+	$uname=$_GET['uname'];
+	$email=$_GET['email'];
+	$pass=$_GET['pass'];
+	dbset();
+	$sql= "INSERT INTO profiles (PRF_FIRSTNAME, PRF_LASTNAME, PRF_NICK, PRF_EMAIL, PRF_PW) VALUES ('$fname', '$lname', '$uname', '$email', '$pass')";
+	$save = mysql_query($sql);
+	echo json_encode($save);
+?>
