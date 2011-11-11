e2852c46d69653d14bcd8c025e2a2977341d6ecb
Stefan Tlach <stefan.tlach@gmail.com>
2011-11-11 10:54:49 +0100

    changed index.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-    // Folgende Zeile im Produktivmodus entfernen:
     defined('YII_DEBUG') or define('YII_DEBUG',true);
-    // Yii-Startdatei einbinden
     require_once('lib/yii/framework/yii.php');
-    // Instanz einer Applikation erzeugen und starten
     $configFile='protected/config/main.php';
     Yii::createWebApplication()->run();
 ?>
